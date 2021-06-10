e2eb7d24cfbf29b9d84b219d115869883c02642f
Quinton Mills <41127568+quintonmills@users.noreply.github.com>
2021-06-10 05:39:19 -0400

    Added Feign client to consume API

---
+++ microservice/currency-exchange-service/src/main/java/com/microservice/currencyexchangeservice/Entity.java
@@ -0,0 +1,5 @@
+package com.microservice.currencyexchangeservice;
+
+public @interface Entity {
+
+}
---
+++ microservice/currency-exchange-service/target/classes/data.sql
@@ -0,0 +1,8 @@
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple,environment)
+values(1000,'USD','INR', 65, '');
+
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple,environment)
+values(1001,'EUR','INR', 25, '');
+
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple,environment)
+values(1002,'AUD','INR', 15, '');
